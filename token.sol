3f75d3f21b2640640eecc1a2506635f7e0bb8841
Apollo18 <40637381+Apollo18Crypto@users.noreply.github.com>
2018-08-30 09:03:54 -0400

    Update token.sol
    minor updates for code verification and proper decimals

--- token.sol
+++ token.sol
@@ -26,14 +26,12 @@ contract TokenERC20 {
      * Initializes contract with initial supply tokens to the creator of the contract
      */
     function TokenERC20(
-        uint256 initialSupply,
-        string tokenName,
-        string tokenSymbol
+
     ) public {
-        totalSupply = initialSupply * 10 ** uint256(decimals);  // Update total supply with the decimal amount
+        totalSupply = 24000000 * 10 ** uint256(decimals);  // Update total supply with the decimal amount
         balanceOf[msg.sender] = totalSupply;                // Give the creator all initial tokens
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
+        name = "Apollo18";                                   // Set the name for display purposes
+        symbol = "A18";                               // Set the symbol for display purposes
     }
 
     /**
